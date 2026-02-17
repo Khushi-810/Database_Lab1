9d6468809172b8c9f295b70c7a74e2d994d74115
Khushi Kumari <Khushijh810@gmail.com>
2026-02-17 23:41:42 +0530

    Update Solutions_Part_A.sql

--- Solutions_Part_A.sql
+++ Solutions_Part_A.sql
@@ -9,33 +9,88 @@ CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
 /* 3. Create Student Table */
-CREATE TABLE Student (
-    RollNo INT,
-    Name VARCHAR(50),
-    Dept VARCHAR(20),
-    Age INT,
-    Phone VARCHAR(15)
+CREATE TABLE Stud1(
+    RollNo NUMBER,
+    Name VARCHAR2(50),
+    Dept VARCHAR2(20),
+    Age NUMBER,
+    Phone VARCHAR2(15)
 );
 
+Table created.
+
+DESC Stud1;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ROLLNO                                             NUMBER
+ NAME                                               VARCHAR2(50)
+ DEPT                                               VARCHAR2(20)
+ AGE                                                NUMBER
+ PHONE                                              VARCHAR2(15)
+
 /* 4. Create Course Table */
-CREATE TABLE Course (
+CREATE TABLE Course1 (
     CourseID INT,
     CourseName VARCHAR(50),
     Credits INT
 );
 
-/* 5. Add City Column */
-ALTER TABLE Student
-ADD City VARCHAR(50);
+Table created.
+ DESC course1;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ COURSEID                                           NUMBER(38)
+ COURSENAME                                         VARCHAR2(50)
+ CREDITS                                            NUMBER(38)
 
+/* 5. Add City Column */
+ALTER TABLE Stud1
+ADD City VARCHAR2(50);
+
+Table altered.
+
+DESC Stud1;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ROLLNO                                             NUMBER
+ NAME                                               VARCHAR2(50)
+ DEPT                                               VARCHAR2(20)
+ AGE                                                NUMBER
+ PHONE                                              VARCHAR2(15)
+ CITY                                               VARCHAR2(50)
 /* 6. Add Semester Column */
-ALTER TABLE Student
-ADD Semester INT;
+ALTER TABLE Stud1
+ADD Semester NUMBER;
+
+Table altered.
+
+DESC Stud1;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ROLLNO                                             NUMBER
+ NAME                                               VARCHAR2(50)
+ DEPT                                               VARCHAR2(20)
+ AGE                                                NUMBER
+ PHONE                                              VARCHAR2(15)
+ CITY                                               VARCHAR2(50)
+ SEMESTER                                           NUMBER
 
 /* 7. Rename Column Phone to MobileNo */
-
-ALTER TABLE Student
+ALTER TABLE Stud1
 RENAME COLUMN Phone TO MobileNo;
+
+Table altered.
+
+DESC Stud1;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ROLLNO                                             NUMBER
+ NAME                                               VARCHAR2(50)
+ DEPT                                               VARCHAR2(20)
+ AGE                                                NUMBER
+ MOBILENO                                           VARCHAR2(15)
+ CITY                                               VARCHAR2(50)
+ SEMESTER                                           NUMBER
 
 /* 8. Drop Course Table */
 DROP TABLE Course;
